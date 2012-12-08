ce36c1cb746a355b78f778a1feb816ac315a1c39
cschirin@googlemail.com <cschirin@googlemail.com@4eff0421-1fc3-aa8a-59d9-67c0278c1301>
2012-12-08 14:49:31 +0000

    weiter an dem relationalem schema gefeilt, gleich gehts los mit ts

--- trunk/app/models/orders_consists_of_parts.rb
+++ trunk/app/models/orders_consists_of_parts.rb
@@ -1,5 +1,5 @@
 class OrdersConsistsOfParts < ActiveRecord::Base
-  attr_accessible :preisBestellung, :stueckzahl
+  attr_accessible :preisBestellung, :stueckzahl, :orderID, :partID
   set_table_name "orders_consists_of_parts" #migration ändern
 
   belongs_to :order
