b4e381e995a8c7f03de01cbe278d0e5be6a05a0e
cschirin@googlemail.com <cschirin@googlemail.com@4eff0421-1fc3-aa8a-59d9-67c0278c1301>
2013-01-10 13:58:39 +0000

    Auftragsverfolgung in Kasse#buy eingebaut und Tabellenbefüllung der Auftragsverfolgung über wechselseitige Rekursion eingebaut.

--- trunk/app/controllers/kasse_controller.rb
+++ trunk/app/controllers/kasse_controller.rb
@@ -16,7 +16,9 @@ class KasseController < ApplicationController
 		
 		
 		produkte = []
-		prodAnzahlHash.each {|pname| produkte <<  Parts.find_by_name(pname) } 	
+		prodAnzahlHash.each {|pname| produkte <<  Parts.find_by_name(pname) } 
+
+
 		
 		produkte.each  do |produkt|
 		
@@ -25,9 +27,74 @@ class KasseController < ApplicationController
 		
 		end 
 		
+
+		trackOrder(o,produkte) 
 		
 		
 		$warenkorbListe = []
-		
+
   end
+  	#Trägt bestellung in Auftragsverfolgung ein
+    def trackOrder(order,productsOfOrder)
+    	EINE_WOCHE_SPÄTER = 7 # einfach  Date + 7
+    	orderDatum = order.datum
+
+    	productsOfOrder.each do |produkt|
+    	
+   
+    	createDemand(order,orderDatum + EINE_WOCHE_SPÄTER)
+
+    	# fulfillDemand(bedarf,orderDatum + EINE_WOCHE_SPÄTER)
+    	end 
+    end	
+
+    def createDemand(teil,datum)
+    	bedarf = Bedarf.new({:TNr => teil.id , :Datum => datum }) #losbildung für Aufträge mit selbem lieferdatum
+    	bedarf.save
+
+    	fulfillDemand(bedarf,datum)
+    end 	
+
+    #Trägt Bedarfsdeckungen für teil an einem datum  ein. 
+    def fulfillDemand(bedarf, datum)
+    	auftrag = erstelleAuftrag(bedarf,datum)
+    	
+    	bedarfsDeckung = BedarfsDeckung.new({ AuTNr:(auftrag.TNr) , BeTNr:(bedarf.TNr) , AuDatum:(auftrag.Datum) , BeDatum:(bedarf.Datum) })
+    	bedarfsDeckung.save
+
+
+
+    end	
+
+    def erstelleAuftrag(auftrag,datum)
+    	auftrag = Auftrag.new({:TNr => auftrag.TNr, :Datum => datum}) 
+    	auftrag.save
+
+    	leiteBedarfeAb(auftrag,datum - 1 ) # Jetzt kommt die Vorlaufsverschiebung rein, beim ableiten von Bedarfen! 
+
+    	auftrag 
+    end 	
+
+    def leiteBedarfeAb(auftrag, datum) 
+
+
+
+    	unterteile = PartsConsistsOfParts.where(:oberteilID => auftrag.TNr)
+
+    #	if (unterteile.empty?) then return; 
+    #	else 
+    		unterteile.each do |t| 
+    			bedarf = createDemand(t, datum ) 
+    			bedarfsAbleitung =  BedarfsAbleitung.new({ AuTNr:(auftrag.TNr) , BeTNr:(bedarf.TNr) , AuDatum:(auftrag.Datum) , BeDatum:(bedarf.Datum) })
+
+	    		bedarfsAbleitung.save
+
+    		end	
+    #	end 	
+
+
+    
+
+
+    end 	
 end
